7506581f9cda84b50be0cb5e72e1996323542117
Rafael Araujo <exodia273@gmail.com>
2022-06-01 19:17:08 -0300

    Adicionando Exercicios

---
+++ algoritimo/atividadePratica3.1.py
@@ -0,0 +1,26 @@
+"""
+Operadores Aritimeticos
+Operador    Ação 
+
++           Adição
+-           Subtração
+*           Multiplicação
+/           Divisão
+//          Divisao inteiro
+**          Potenciação
+%           REsto de divisao
+()          Procedencia
+
+
+"""
+
+print('Adição: 2 + 5 = {}'.format(2 + 5))
+print('Subtração: 2 - 5 = {}'.format(2 - 5))
+print('Multiplicação: 2 * 5 = {}'.format(2 * 5))
+print('Divisão: 2 / 5 = {}'.format(2 / 5))
+print('Divisao inteiro: 22 // 5 = {}'.format(22 // 5))
+print('Potenciacao: 2 ** 8 = {}'.format(2 ** 8))
+print('Resto: 2 % 5 = {}'.format(2 % 5))
+print('Sem procedencia: 5 + 2 * 10 = {}'.format(5 + 2 * 10))
+print('Com procedencia:(5 + 2) * 10 = {}'.format((5 + 2) * 10))
+
---
+++ algoritimo/exercicioFormat.py
@@ -0,0 +1,13 @@
+"""
+Reescreva o programa demonstrando adequando-o ao uso da tecnica F.format de formatação
+de String
+
+"""
+def hello(meu_nome):
+    print("Ola", meu_nome)
+
+
+hello("Rafael")
+
+teste = input("Qual seu nome?")
+print("Seu nome e ", teste)
