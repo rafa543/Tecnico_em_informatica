182e9f202d25a42b98d065f41d77a37498d7f45e
Rafael Araujo <exodia273@gmail.com>
2022-07-14 21:20:06 -0300

    aula dia 14/07/2022

--- AULAS/aula15_listas.py
+++ AULAS/aula15_listas.py
@@ -22,3 +22,29 @@ print(f'lista1: {lista1}')
 print(f'lista2: {lista2}')
 print(f'Valor maximo lista1: {max(lista1)}')
 print(f'Valor maximo lista2: {max(lista2)}')
+
+
+lista1 = [1, 2, 3, 4, 5, 6]
+lista2 = [7, 8, 9, 10, 11, 12]
+
+print(f'Lista1: {lista1}')
+print(f'Lista2: {lista2}')
+
+lista1.append('a')
+print('append:', lista1)
+
+lista2.insert(0, "Chocolate")
+print('insert: ', lista2)
+
+lista1.pop()
+print(lista1)
+
+del(lista2[-1])
+
+print("del: ", lista2)
+
+lista2.remove("Chocolate")
+print(lista2)
+
+# lista2.clear()
+# print(lista1)
